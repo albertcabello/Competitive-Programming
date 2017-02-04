8aadc4b4e150bd3dc4f5a8415ab2a265089ed804
albertcabello <albert.a.cabello@gmail.com>
2017-02-04 10:52:58 -0500

    Initial Commit

---
+++ Programming Team/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Programming Team
+//
+//  Created by Alberto Cabello on 2/4/17.
+//  Copyright © 2017 Alberto Cabello. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
