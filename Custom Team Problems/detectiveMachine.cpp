aa31ef35e68061fd212d863931779441049c9a7d
Alberto Cabello <albertcabello@albertos-mbp.attlocal.net>
2017-07-17 09:32:45 -0400

    Finished Detective Machine

--- Custom Team Problems/detectiveMachine.cpp
+++ Custom Team Problems/detectiveMachine.cpp
@@ -9,38 +9,39 @@ int main() {
 	cin >> c;
 	while(c--) {
 		int count;
+		cin >> count;
 		list<string> path;
 		map<string, pair<string, string> > people;
 		string name, blame, investigate;
 		cin >> name >> blame >> investigate;
 		people[name] = make_pair(blame, investigate); //Ignore blame here because it's James
+		count--; //We're never going to see james;
 		while (people.size() != count) {
 			cin >> name >> blame >> investigate;
 			people[name] = make_pair(blame, investigate);
-			cout << "Loop" << endl;
 		}
-		auto begin = path.begin();
-		*begin = "Margarita";
-		auto end = path.end();
-		*end = "James";
-		auto pos = path.begin();
+		path.push_back("Margarita");
 		pair<string, string> curPerson = people["Margarita"];
 		set<string> visited;
 		visited.insert("Margarita");
-		while (curPerson.first != "James") {
-			path.insert(pos, curPerson.first);
-			if (visited.find(curPerson.second) != visited.end()) {
-				curPerson = people[curPerson.second];
+		bool broke = 0;
+		do {
+			path.insert(path.end(), curPerson.second);
+			curPerson = people[curPerson.second];
+			if (visited.find(curPerson.second) == visited.end()) {
 				visited.insert(curPerson.second);
 			}
 			else {
-				cout << -1 << endl;
+				broke = 1;
 				break;
 			}
-			pos++;
+		} while (curPerson.first != "James" && curPerson.second != "James");
+		path.push_back("James");
+		if (!broke) {
+			cout << path.size()-2 << endl;
 		}
-		for (auto it = path.begin(); it != path.end(); ++it) {
-			cout << *it << " ";
+		else {
+			cout << -1 << endl;
 		}
 	}
 	return 0;
